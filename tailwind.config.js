58faebb63b05dd5815311cb1a6941c3b9e0e5ae5
Your Name <rajatpandey542@gmail.com>
2026-01-09 20:06:30 +0530

    git config file added

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./index2.html",
+    "./script.js",
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        jersey: ['"Jersey 10"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
