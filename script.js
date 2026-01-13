60332dd864690cf69ff88920f00e4a00d7d54121
Your Name <rajatpandey542@gmail.com>
2026-01-13 20:26:16 +0530

    Feature : add experience block in the portfolio

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-const slider = document.getElementById("slider");
-const slides = slider.children.length;
-let index = 0;
 
-setInterval(() => {
-  index = (index + 1) % slides;
-  slider.style.transform = `translateX(-${index * 100}%)`;
-}, 2000);
+  document.addEventListener("DOMContentLoaded", function () {
+    let currentIndex = 0;
+    const slider = document.getElementById("slider");
+    const totalSlides = slider.children.length;
+
+    function updateSlide() {
+      slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
+
+    window.nextSlide = function () {
+      currentIndex = (currentIndex + 1) % totalSlides;
+      updateSlide();
+    };
+
+    window.prevSlide = function () {
+      currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+      updateSlide();
+    };
+  });
+
+
+
+
 
 
 
